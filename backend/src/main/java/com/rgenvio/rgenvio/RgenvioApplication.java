e95d59be576d73c4134e28d58c9d9685b496bf6a
Renato Galdino <reygaldino.rg@gmail.com>
2022-07-15 14:35:32 -0300

    Criação Inicial do Projeto SMS

---
+++ backend/src/main/java/com/rgenvio/rgenvio/RgenvioApplication.java
@@ -0,0 +1,13 @@
+package com.rgenvio.rgenvio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgenvioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgenvioApplication.class, args);
+	}
+
+}

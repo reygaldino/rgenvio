6cfd05e33314a130a2b92d90ef249f8a39e64a50
Renato Galdino <reygaldino.rg@gmail.com>
2022-07-17 12:03:08 -0300

    Envio de SMS

--- backend/src/main/java/com/rgenvio/rgenvio/controller/ClientController.java
+++ backend/src/main/java/com/rgenvio/rgenvio/controller/ClientController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rgenvio.rgenvio.entities.Client;
 import com.rgenvio.rgenvio.services.ClientService;
+import com.rgenvio.rgenvio.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/client")
@@ -17,6 +19,9 @@ public class ClientController {
 	
 	@Autowired
 	private ClientService service;
+	
+	@Autowired
+	private SmsService smsService;
 
 	@GetMapping
 	public Page<Client> findClient(
@@ -24,5 +29,10 @@ public class ClientController {
 			@RequestParam(value = "maxDate",defaultValue = "") String maxDate ,
 			Pageable pageable){
 		return service.findClients(minDate,maxDate,pageable);
+	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms (@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 }
